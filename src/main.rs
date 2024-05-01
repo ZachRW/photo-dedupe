ce6f298f6ce84e933c10fa00b04a100799416dc1
Zachary Roberts-Weigert <zachrw123@gmail.com>
2024-05-01 03:23:50 -0400

    add dummy implementation of photo merging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod group;
 
-use std::{fs, process::Command};
+use std::{collections::HashMap, fs, process::Command};
 
 use anyhow::{anyhow, Result};
 use group::{get_duplicate_groups, Group};
+use indicatif::ProgressBar;
 
 fn main() -> Result<()> {
     // Read duplicate sets
@@ -12,31 +13,61 @@ fn main() -> Result<()> {
         99,
     )?;
 
-    let groups = groups
-        .into_iter()
-        .filter(|group| group.files.iter().all(is_jpeg));
+    let groups: Vec<&Group> = groups
+        .iter()
+        .filter(|group| group.files.iter().all(is_jpeg)).collect();
+    let mut file_aliases = HashMap::default();
 
-    for group in groups {
-        let (largest_file_index, largest_file) = group
-            .files
-            .iter()
-            .enumerate()
-            .max_by_key(|&(_, path)| get_file_size(path))
-            .ok_or(anyhow!("Empty group"))?;
-
-        let mut other_files: Vec<&String> = group.files.iter().collect();
-        other_files.remove(largest_file_index);
-        other_files.sort_unstable_by_key(|&path| get_metadata_size(path));
-
-        
+    let bar = ProgressBar::new(groups.len() as u64);
+    
+    // Merge metadata into largest file copy
+    for group in bar.wrap_iter(groups.into_iter()) {
+        merge_group(group, &mut file_aliases)?;
     }
 
-    // Merge metadata into largest file copy
     // Read .lnk albums
     // Redirect file locations to largest
     // Store albums in text files
 
     Ok(())
+}
+
+fn merge_group<'a>(
+    group: &'a Group,
+    file_aliases: &mut HashMap<&'a String, &'a String>,
+) -> Result<()> {
+    println!("finding largest file");
+    let (largest_file_index, largest_file) = group
+        .files
+        .iter()
+        .enumerate()
+        .max_by_key(|&(_, path)| get_file_size(path))
+        .ok_or(anyhow!("Empty group"))?;
+
+    println!("sorting files by metadata size");
+    let mut other_files: Vec<&String> = group.files.iter().collect();
+    other_files.remove(largest_file_index);
+    other_files.sort_unstable_by_key(|&path| get_metadata_size(path));
+    println!("done sorting");
+
+    let largest_file_copy = create_temp_copy(&largest_file)?;
+    for file in other_files {
+        merge_metadata(file, largest_file);
+        file_aliases.insert(file, largest_file);
+    }
+    merge_metadata(&largest_file_copy, largest_file);
+
+    Ok(())
+}
+
+fn merge_metadata(source: &String, destination: &String) {
+    println!("merging metadata from `{}` into `{}`", source, destination);
+    println!("deleting `{}`", source);
+}
+
+fn create_temp_copy(file: &String) -> Result<String> {
+    println!("creating temp copy of `{}`", file);
+    Ok(format!("{}_temp", file))
 }
 
 fn is_jpeg(path: &String) -> bool {
@@ -49,11 +80,9 @@ fn get_file_size(path: &String) -> u64 {
 
 fn get_metadata_size(path: &String) -> usize {
     let metadata = Command::new("exiftool")
-        .arg("-v")
+        .arg("-b")
         .arg(path)
         .output()
         .expect("Failed to get exif data");
     metadata.stdout.len()
 }
-
-fn merge_metadata(group: &Group) {}
